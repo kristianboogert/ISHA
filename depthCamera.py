7eace35addd4d517eac1ea3b5077d33c54878167
Kristian Boogert <kristian.boogert@gladi8tor.com>
2023-02-21 11:51:29 +0100

    code modularity

--- depthCamera.py
+++ depthCamera.py
@@ -33,3 +33,39 @@ class DepthCamera:
         #     pass
         cv2.putText(resized_color_image, "+", TEST_POSITION, cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
         cv2.imshow('RealSense color', resized_color_image)
+
+    def display_distance_frame(self, depth_frame, position):
+        x, y = position
+        depth_image = np.asanyarray(depth_frame.get_data())
+
+        resized_depth_image = cv2.resize(depth_image, dsize=self.resolution)  #, interpolation=cv2.INTER_AREA)
+        dist = (resized_depth_image[y][x]/100)-1.06299213
+        print("HARDWARE Z: {} cm".format(dist*2.54))
+        # cv2.imshow('RealSense depth', resized_depth_image)
+
+    # Callback function to process a frame
+    def process(self, rgb_d_frame):
+        # Extract color frame and depth frame from frame
+        color_frame = rgb_d_frame.get_color_frame()
+        depth_frame = rgb_d_frame.get_depth_frame()
+        if not depth_frame or not color_frame:
+            # print("Skipping frame due to missing data")
+            return
+
+        self.display_color_frame(color_frame)
+        self.display_distance_frame(depth_frame, TEST_POSITION)
+
+    def run(self):
+        self.pipeline.start(self.config)
+        try:
+            while True:
+                rgb_d_frame = self.pipeline.wait_for_frames()
+                self.process(rgb_d_frame)
+                if cv2.waitKey(5) & 0xFF == ord('q'):
+                    self.exit()
+        finally:
+            self.exit()
+
+    def exit(self):
+        self.pipeline.stop()
+        cv2.destroyAllWindows()
