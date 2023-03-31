a6665878a2ee4b927820f9c2bb0b2b4a7d5b656d
Kristian Boogert <kristian.boogert@gladi8tor.com>
2023-03-31 14:41:07 +0200

    safety commit

---
+++ database/db_connection.py
@@ -0,0 +1,19 @@
+import mysql.connector
+from getpass import getpass
+from mysql.connector import Error
+import pandas as pd
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
