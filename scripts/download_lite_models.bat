d2b7a98e0bdb4727d6fb16630aff0bcd20ae8118
Tom <tomniesse@gmail.com>
2023-05-02 16:07:56 +0200

    scripts bijgewerkt

--- scripts/download_lite_models.bat
+++ scripts/download_lite_models.bat
@@ -1,1 +1,7 @@
+echo "THIS SCRIPT ASSUMES YOU ARE USING PYTHON 3.9. IF THIS IS NOT THE CASE, OPEN THIS FILE WITH NOTEPAD AND EDIT THE PYTHON VERSION NUMBER!"
+echo "THIS SCRIPT ALSO ASSUMES YOU HAVE WGET INSTALLED! IF THIS IS NOT THE CASE, INSTALL IT FIRST!"
+echo ""
+echo "Press enter to start"
+pause
 wget https://storage.googleapis.com/mediapipe-assets/pose_landmark_lite.tflite -O c:\python39\lib\site-packages\mediapipe\modules\pose_landmark\pose_landmark_lite.tflite
+wget https://storage.googleapis.com/mediapipe-assets/hand_landmark_lite.tflite -O c:\python39\lib\site-packages\mediapipe\modules\hand_landmark\hand_landmark_lite.tflite
