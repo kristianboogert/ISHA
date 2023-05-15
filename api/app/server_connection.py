a2340546ea97114575417c98c59080beb8f3d53d
Kristian Boogert <kristian.boogert@gladi8tor.com>
2023-05-16 00:51:58 +0200

    database api wip

---
+++ api/app/server_connection.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from getpass import getpass
+from mysql.connector import Error
+import pandas as pd
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
