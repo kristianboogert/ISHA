c7697e414bc68350aa2e2ad98b00153eec622edc
Kristian Boogert <kristian.boogert@gladi8tor.com>
2023-05-16 11:43:58 +0200

    updated schemas

--- api/app/routers/post.py
+++ api/app/routers/post.py
@@ -10,7 +10,13 @@ router = APIRouter()
 def root():
     return {"message": "Hello World"}
 
-# Create 
+# Get users
+@router.get("/user")
+def get_user(db: Session = Depends(get_db)):
+    posts = db.query(models.User).all()
+    return posts
+
+# Create user
 @router.post("/user", status_code = status.HTTP_201_CREATED)
 def create_posts(user: schemas.UserCreate, db: Session = Depends(get_db)):
     new_post = models.User(**user.dict())
@@ -19,13 +25,7 @@ def create_posts(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_post)
     return new_post
 
-# Get user
-@router.get("/user")
-def get_user(db: Session = Depends(get_db)):
-    posts = db.query(models.User).all()
-    return posts
-
-# Get post by id
+# Get user by id
 @router.get("/user/{id}")
 def get_post(id: int, db: Session = Depends(get_db)):
     posts = db.query(models.User).filter(models.User.id == id).first()
@@ -33,7 +33,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return posts
 
-# Delete by id
+# Delete user by id
 @router.delete("/user/{id}", status_code = status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
     posts = db.query(models.User).filter(models.User.id == id)
@@ -43,8 +43,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-
-# Update by id
+# Update user by id
 @router.put("/user/{id}")
 def update_post(id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
     posts_query = db.query(models.User).filter(models.User.id == id)
@@ -54,14 +53,116 @@ def update_post(id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)
     posts_query.update(user.dict(), synchronize_session=False)
     return{"data": posts_query.first()}
 
-# @router.post("/exercise", status_code = status.HTTP_201_CREATED)
-# def create_posts(post: schemas.exercise):
-#     post_dict = post.dict()
-#     exercise.append(post_dict)
-#     return {"data": post_dict}
 
-# @router.post("/score", status_code = status.HTTP_201_CREATED)
-# def create_posts(post: score):
-#     post_dict = post.dict()
-#     score.append(post_dict)
-#     return {"data": post_dict}
+# Create exercise
+@router.post("/exercise", status_code = status.HTTP_201_CREATED)
+def create_posts(exercise: schemas.ExerciseCreate, db: Session = Depends(get_db)):
+    new_post = models.Exercise(**exercise.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+# Get exercise by id
+@router.get("/exercise/{id}")
+def get_post(id: int, db: Session = Depends(get_db)):
+    posts = db.query(models.Exercise).filter(models.Exercise.id == id).first()
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return posts
+
+# Delete exercise by id
+@router.delete("/exercise/{id}", status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    posts = db.query(models.Exercise).filter(models.Exercise.id == id)
+    if posts.first() == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
+    posts.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+# Update exercise by id
+@router.put("/user/{id}")
+def update_post(id: int, exercise: schemas.ExerciseUpdate, db: Session = Depends(get_db)):
+    posts_query = db.query(models.Exercise).filter(models.Exercise.id == id)
+    posts = posts_query.first()
+    if posts == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND) 
+    posts_query.update(exercise.dict(), synchronize_session=False)
+    return{"data": posts_query.first()}
+
+
+# Create score
+@router.post("/score", status_code = status.HTTP_201_CREATED)
+def create_posts(score: schemas.ScoreCreate, db: Session = Depends(get_db)):
+    new_post = models.Score(**score.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+# Get exercise by id
+@router.get("/score/{id}")
+def get_post(id: int, db: Session = Depends(get_db)):
+    posts = db.query(models.Score).filter(models.Score.id == id).first()
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return posts
+
+# Delete exercise by id
+@router.delete("/score/{id}", status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    posts = db.query(models.Score).filter(models.Score.id == id)
+    if posts.first() == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
+    posts.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+# Update score by id
+@router.put("/score/{id}")
+def update_post(id: int, score: schemas.ScoreUpdate, db: Session = Depends(get_db)):
+    posts_query = db.query(models.Score).filter(models.Score.id == id)
+    posts = posts_query.first()
+    if posts == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND) 
+    posts_query.update(score.dict(), synchronize_session=False)
+    return{"data": posts_query.first()}
+
+
+# Create metadata
+@router.post("/metadata", status_code = status.HTTP_201_CREATED)
+def create_posts(metadata: schemas.MetadataCreate, db: Session = Depends(get_db)):
+    new_post = models.Metadata(**metadata.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+# Get metadata by id
+@router.get("/metadata/{id}")
+def get_post(id: int, db: Session = Depends(get_db)):
+    posts = db.query(models.Metadata).filter(models.Metadata.id == id).first()
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return posts
+
+# Delete metadata by id
+@router.delete("/metadata/{id}", status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    posts = db.query(models.Metadata).filter(models.Metadata.id == id)
+    if posts.first() == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
+    posts.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+# Update metadata by id
+@router.put("/metadata/{id}")
+def update_post(id: int, metadata: schemas.MetadataUpdate, db: Session = Depends(get_db)):
+    posts_query = db.query(models.Metadata).filter(models.Metadata.id == id)
+    posts = posts_query.first()
+    if posts == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND) 
+    posts_query.update(metadata.dict(), synchronize_session=False)
+    return{"data": posts_query.first()}
--- api/app/schemas.py
+++ api/app/schemas.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 
 class UserBase(BaseModel):
-    id:         int
-    name:       str
+    id:             int
+    name:           str
 
 class UserCreate(UserBase):
     pass
@@ -12,10 +12,14 @@ class UserCreate(UserBase):
 class UserUpdate(UserBase):
     pass
 
+class UserDelete(UserBase):
+    pass
+
 class ExerciseBase(BaseModel):
-    id:         int
-    user_id:    int
-    score_id:   int
+    id:             int
+    name:           str
+    type:           str
+    description:    str
 
 class ExerciseCreate(ExerciseBase):
     pass
@@ -23,11 +27,14 @@ class ExerciseCreate(ExerciseBase):
 class ExerciseUpdate(ExerciseBase):
     pass
 
+class ExerciseDelete(ExerciseBase):
+    pass
+
 class ScoreBase(BaseModel):
-    id:         int
-    exercise_id:int 
-    user_id:    int
-    value:      str   
+    id:             int
+    exercise_id:    int 
+    user_id:        int
+    value:          str   
 
 class ScoreCreate(ScoreBase):
     pass
@@ -35,3 +42,23 @@ class ScoreCreate(ScoreBase):
 class ScoreUpdate(ScoreBase):
     pass
 
+class ScoreDelete(ScoreBase):
+    pass
+
+class MetadataBase(BaseModel):
+    id:                 int
+    bodypart_name:      str
+    bodypart_angle_xy:  str
+    bodypart_angle_yz:  str
+    bodypart_angle_xz:  str
+    created_at:         datetime
+    score_id:           int
+
+class MetadataCreate(MetadataBase):
+    pass
+
+class MetadataUpdate(MetadataBase):
+    pass
+
+class MetadataDelete(MetadataBase):
+    pass
