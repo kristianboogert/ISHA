8835f6a4e1340f43ed74dcc6277c76477a7f15b9
Tom Niesse <tomniesse@gmail.com>
2023-05-23 09:38:18 +0200

    ongeteste api client

--- src/main.py
+++ src/main.py
@@ -41,30 +41,47 @@ def main():
 
 
 
-    # Read JSON containing an exerciseDescription
-    # exerciseDescriptionFilepath = "./exerciseDescriptions/arm_to_side.json"    # body pose exercise demo
-    # exerciseDescriptionFilepath = "./exerciseDescriptions/hand_rotation.json"  # hand rotation exercise demo
-    exerciseDescriptionFilepath = "./exerciseDescriptions/fist.json"           # hand exercise demo
+    # # Read JSON containing an exerciseDescription
+    # # exerciseDescriptionFilepath = "./exerciseDescriptions/arm_to_side.json"    # body pose exercise demo
+    # # exerciseDescriptionFilepath = "./exerciseDescriptions/hand_rotation.json"  # hand rotation exercise demo
+    # exerciseDescriptionFilepath = "./exerciseDescriptions/fist.json"           # hand exercise demo
 
-    exerciseDescription = open(exerciseDescriptionFilepath).read()
-    print(exerciseDescription)
-    # Convert the exerciseDescription to exerciseData, so the pose detection can just follow instructions,
-    # without having any real world knowlegde
-    exerciseData = ExerciseDataCreator.createExerciseData(exerciseDescription, ImpairedSideType.LEFT)
-    # Initialize camera. If camera.start() is not called, it will not give frames. Same goes for camera.stop()
-    camera = Camera(cameraId=0)
-    camera.start()
-    # Initialize pose detections
-    bodyPoseDetection = BodyPoseDetection()
-    handPoseDetection = HandPoseDetection()
-    # Try to get a score by looking at a user's movements
-    print(exerciseData)
-    score, pose_metadata = FuglMeyer.scoreExercise(camera, bodyPoseDetection, handPoseDetection, exerciseData)
+    # exerciseDescription = open(exerciseDescriptionFilepath).read()
+    # print(exerciseDescription)
+    # # Convert the exerciseDescription to exerciseData, so the pose detection can just follow instructions,
+    # # without having any real world knowlegde
+    # exerciseData = ExerciseDataCreator.createExerciseData(exerciseDescription, ImpairedSideType.LEFT)
+    # # Initialize camera. If camera.start() is not called, it will not give frames. Same goes for camera.stop()
+    # camera = Camera(cameraId=0)
+    # camera.start()
+    # # Initialize pose detections
+    # bodyPoseDetection = BodyPoseDetection()
+    # handPoseDetection = HandPoseDetection()
+    # # Try to get a score by looking at a user's movements
+    # print(exerciseData)
+    # score, pose_metadata = FuglMeyer.scoreExercise(camera, bodyPoseDetection, handPoseDetection, exerciseData)
+    # print(pose_metadata)
+    # print(score)
+
+    pose_metadata_txt = open("real_exercise_data.json").read()
+    print(pose_metadata_txt)
+    pose_metadata = json.loads(pose_metadata_txt)
     print(pose_metadata)
-    print(score)
-
     # url moet nog verbeterd, maar zou moeten werken
     url = '127.0.0.1:8000/metadata'
-    response = requests.post(url, json = pose_metadata)
-    print(response.text)
+    for exercisePart in pose_metadata["exercise_parts"]:
+        for bodyPart in exercisePart:
+            dict_thing = {
+                "bodypart_name": bodyPart["body_part"],
+                "bodypart_angle_xy": bodyPart["angles"]["xy"],
+                "bodypart_angle_yz": bodyPart["angles"]["yz"],
+                "bodypart_angle_xz": bodyPart["angles"]["xz"],
+                "score_id", 0 # TODO: maak in de toekomst eerst score aan!
+            }
+            # make the post request
+            response = requests.post(url, json = json)
+            if response is not None:
+                print(response.text)
+            else:
+                print("alles ging fout")
 main()
